4d14e370a1aa059766764f350443daddfad17b43
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-05-16 16:42:47 +0500

    UI complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
      <div className="input-section">
        <TextField variant='filled' color='secondary' label='Enter a number' className='input' />
        <Button variant='contained' color='secondary' className='input'>Check</Button>
+     </div>
+     <div className="result">
+       <h1 className='res-tag' >PRIME</h1>
      </div>
     </div>
   );
