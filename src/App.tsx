b04f334d91219072886721119fa65f61514f76ca
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-05-16 16:33:26 +0500

    Inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core';
 import TextField  from '@material-ui/core/TextField';
 import React from 'react';
 import './App.css';
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <div className="input-section">
        <TextField variant='filled' color='secondary' label='Enter a number' className='input' />
+       <Button variant='contained' color='secondary' className='input'>Check</Button>
      </div>
     </div>
   );
